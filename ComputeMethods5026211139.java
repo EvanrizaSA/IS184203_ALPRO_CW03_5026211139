30f9e877b98025d6049831f1a755f9c12dacb676
EvanrizaSA <99796945+EvanrizaSA@users.noreply.github.com>
2022-02-28 13:43:45 +0700

    Add files via upload

--- ComputeMethods5026211139.java
+++ ComputeMethods5026211139.java
@@ -2,14 +2,19 @@ import java.util.Random;
 
 public class ComputeMethods5026211139{
 	
-	public double fToC(double degreesF)	{
+    //Temperature
+	public double fToC(double degreesF){
 
 		return 5/9.0*(degreesF - 32);
 	}
-	public double hypotenuse ( int a, int b)	{
+
+    //Hypotenuse
+	public double hypotenuse ( int a, int b){
 
 		return Math.sqrt((a*a) + (b*b));
 	}
+
+    //Dice-rolling
 	public int roll ()	{
 		Random rndm = new Random();
 		int diceA = rndm.nextInt(6)+1;
